9e4e6efe577b5472c9d6e507d23fbbcf0564071e
Marmhar <salaparejhonmhar@gmail.com>
2026-02-22 23:25:42 +0800

    feat(join): implement INNER JOIN; more cleaner and updated version

--- 01_Foundations/12_Inner_Joins.sql
+++ 01_Foundations/12_Inner_Joins.sql
@@ -4,3 +4,8 @@ SELECT Groupmates.Name, Groupmates.Role, Projects.`ProjectName`
 FROM Groupmates
 INNER JOIN Projects
 ON Groupmates.ProjectID = Projects.ProjectID;
+
+SELECT g.Name, g.Role, p.ProjectName
+FROM Groupmates as g
+INNER JOIN Projects AS p
+ON g.ProjectID = p.ProjectID;
