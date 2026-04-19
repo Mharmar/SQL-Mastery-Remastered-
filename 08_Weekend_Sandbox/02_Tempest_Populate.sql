29e3d9d519cb5ae6ad03ddee7965298ae492be46
Marmhar <salaparejhonmhar@gmail.com>
2026-04-19 22:58:21 +0800

    feat(sandbox): populate Citizens table with initial Tempest Federation data

---
+++ 08_Weekend_Sandbox/02_Tempest_Populate.sql
@@ -0,0 +1,8 @@
+USE Jura_Tempest;
+
+INSERT INTO Citizens (CitizenID, Name, Species, Disaster_Rank)
+VALUES
+(1, 'Rimuru Tempest', 'Demon Slime', 'Catastrophe'),
+(2, 'Benimaru', 'Fair Oni', 'Hazard'),
+(3, 'Shion', 'Fair Oni', 'Hazard'),
+(4, 'Gobta', 'Hobgoblin', 'Normal');
